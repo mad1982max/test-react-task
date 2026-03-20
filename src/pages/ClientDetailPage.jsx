49ad7dd7e7e0fa13c58b719f7d9be5c7aaf0120d
Maksym Bezrodnyi <maks.bezrodnyi@gmail.com>
2026-03-20 14:12:50 +0200

    fix : fix link and styles

--- src/pages/ClientDetailPage.jsx
+++ src/pages/ClientDetailPage.jsx
@@ -1,35 +1,33 @@
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Card, CardContent, Typography, Button } from "@mui/material";
-import {useClientStore} from "../stores/useClientStore";
+import { useClientStore } from "../stores/useClientStore";
+import { ROUTE } from '../constants/routes';
 
 export default function ClientDetailPage() {
-  const { id } = useParams();
-  const navigate = useNavigate();
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  const { clients } = useClientStore();
+    const { clients } = useClientStore();
 
-  console.log({id,clients});
+    const client = clients.find((client) => String(client.id) === String(id));
+    if (!client) return <div>Client not found</div>;
 
-  const client = clients.find((c) => String(c.id) === String(id));
+    return (
+        <Card sx={{ m: 3 }}>
+            <CardContent>
+                <Typography variant="h5">
+                    {client.firstName} {client.lastName}
+                </Typography>
 
-  if (!client) return <div>Client not found</div>;
+                <Typography>Email: {client.email}</Typography>
+                <Typography>Status: {client.status}</Typography>
+                <Typography>Balance: ${client.balance.toFixed(2)}</Typography>
 
-  return (
-    <Card sx={{ m: 3 }}>
-      <CardContent>
-        <Typography variant="h5">
-          {client.firstName} {client.lastName}
-        </Typography>
-
-        <Typography>Email: {client.email}</Typography>
-        <Typography>Status: {client.status}</Typography>
-        <Typography>Balance: ${client.balance.toFixed(2)}</Typography>
-
-        <Button sx={{ mt: 2 }} onClick={() => navigate("/clients")}>
-          Back
-        </Button>
-      </CardContent>
-    </Card>
-  );
+                <Button sx={{ mt: 2 }} onClick={() => navigate(ROUTE.CLIENTS)}>
+                    Back
+                </Button>
+            </CardContent>
+        </Card>
+    );
 }
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ClientsPage from '../pages/ClientsPage';
 import ClientDetailPage from '../pages/ClientDetailPage';
 import NotFoundPage from '../pages/NotFoundPage';
+import { ROUTE } from '../constants/routes';
+
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to="/clients" replace />} />
+                <Route path={ROUTE.HOME} element={<Navigate to={ROUTE.CLIENTS} replace />} />
 
-                <Route path="/clients" element={<ClientsPage />} />
+                <Route path={ROUTE.CLIENTS} element={<ClientsPage />} />
 
-                <Route path="/clients/:id" element={<ClientDetailPage />} />
+                <Route path={`${ROUTE.CLIENTS}/:id`} element={<ClientDetailPage />} />
 
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
